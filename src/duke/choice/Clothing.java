d7c544144b957a47ea904d1fb0531870a29bb94f
opc <opc@java-basics-2.subnet03281847.vcn03281847.oraclevcn.com>
2021-04-21 05:37:01 +0000

    Test1

---
+++ src/duke/choice/Clothing.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duke.choice;
+
+/**
+ *
+ * @author opc
+ */
+public class Clothing {
+    
+    String description;
+    double price;
+    String size = "M";
+
+    
+}
---
+++ src/duke/choice/Customer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duke.choice;
+
+/**
+ *
+ * @author opc
+ */
+public class Customer {
+    
+    String  name;
+    String  size;
+
+    
+    
+    
+}
